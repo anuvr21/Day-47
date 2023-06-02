3c9c7ac3e36202542cc1cd13a31336a67cab687c
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-06-02 15:07:47 +0530

    Add files via upload

---
+++ EndOfFile/EndOfFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int lineNumber = 1;
+
+        while (true)
+        {
+            string line = Console.ReadLine();
+
+            if (line == null)
+                break;
+
+            Console.WriteLine($"{lineNumber} {line}");
+            lineNumber++;
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ccException_Qstn2
+{
+    class MyCalculator
+    {
+        public int Power(int n, int p)
+        {
+            if (n < 0 || p < 0)
+            {
+                throw new Exception("n and p should be non-negative");
+            }
+            else if (n == 0 && p == 0)
+            {
+                throw new Exception("n and p should not be zero");
+            }
+            else
+            {
+                return (int)Math.Pow(n, p);
+            }
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    string input = Console.ReadLine();
+                    string[] numbers = input.Split(' ');
+                    int n = int.Parse(numbers[0]);
+                    int p = int.Parse(numbers[1]);
+                    MyCalculator myCalculator = new MyCalculator();
+                    int result = myCalculator.Power(n, p);
+                    Console.WriteLine(result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
